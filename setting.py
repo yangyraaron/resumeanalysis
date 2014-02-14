06377a40d2299431d0d5f977e585acadba2d4be3
wisedulab2 <yangyr.aaron@gmail.com>
2014-02-14 10:20:18 +0800

    remove console print

--- setting.py
+++ setting.py
@@ -69,7 +69,7 @@ logging = {
     },
     'loggers': {
         app['name']: {
-            'handlers': ['console','info_file_handler', 'error_file_handler'],
+            'handlers': ['info_file_handler', 'error_file_handler'],
             'propagate': True,
             'level': 'DEBUG',
         },
